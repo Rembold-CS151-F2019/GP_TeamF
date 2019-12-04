baae9ef2da39eb24bf248f3a01bd1c5afacdf9b7
bryceoconnor <54556675+bryceoconnor@users.noreply.github.com>
2019-12-04 13:34:08 -0800

    Add files via upload

--- Player_class.py
+++ Player_class.py
@@ -20,15 +20,24 @@ class Player:
         self.key = key
         # counter for every player move made
         moves = 0
-        if key == "Up" and m.Maze.check_wall != "wall":
+        if key == "Up" and m.Maze.check_wall != "wall" or key == "w" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(0, -5)
-        if key == "Down" and m.Maze.check_wall != "wall":
+        if key == "Down" and m.Maze.check_wall != "wall" or key == "s" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(0, 5)
-        if key == "Left" and m.Maze.check_wall != "wall":
+        if key == "Left" and m.Maze.check_wall != "wall" or key == "a" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(-5, 0)
-        if key == "Right" and m.Maze.check_wall != "wall":
+        if key == "Right" and m.Maze.check_wall != "wall" or key == "d" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(5, 0)
+            
+w = g.GraphWin("Window", 500, 500)
+w.setBackground("white")
+p = Player(w, 250, 250)
+key = "z"
+while key != "q":
+    key = w.checkKey()
+    p.control(key)
+w.close()
