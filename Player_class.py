819cd4642f45666b2bc3060b6d7bc6e2af40d116
bryceoconnor <54556675+bryceoconnor@users.noreply.github.com>
2019-12-04 13:27:44 -0800

    Add files via upload

--- Player_class.py
+++ Player_class.py
@@ -1,6 +1,6 @@
 import graphics as g
 import numpy as np
-import maze_class as maze
+import maze_class as m
 
 class Player:
     def __init__(self, w, x, y):
@@ -20,15 +20,15 @@ class Player:
         self.key = key
         # counter for every player move made
         moves = 0
-        if key == "Up" and maze.check_wall != "wall":
+        if key == "Up" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(0, -5)
-        if key == "Down" and maze.check_wall != "wall":
+        if key == "Down" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(0, 5)
-        if key == "Left" and maze.check_wall != "wall":
+        if key == "Left" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(-5, 0)
-        if key == "Right" and maze.check_wall != "wall":
+        if key == "Right" and m.Maze.check_wall != "wall":
             moves += 1
             self.object.move(5, 0)
