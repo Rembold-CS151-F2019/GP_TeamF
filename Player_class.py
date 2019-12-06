18da013f92ce3f4215b68fbef1b8b9242432f741
bryceoconnor <54556675+bryceoconnor@users.noreply.github.com>
2019-12-05 22:00:16 -0800

    Add files via upload

--- Player_class.py
+++ Player_class.py
@@ -2,7 +2,6 @@ import graphics as g
 #import numpy as np
 # Importing Isa's class to use
 import maze_class as m
-#import Control as k
 
 class Player:
     def __init__(self, w, x, y):
@@ -10,6 +9,7 @@ class Player:
         self.x = x
         self.y = y
         self.draw()
+        self.location = self.object.getCenter()
         
     # Making the visual representation of the player
     def draw(self):
@@ -23,15 +23,33 @@ class Player:
         # counter for every player move made
         moves = 0
         # Allows for either arrows or WASD movement
-        if key == "Up" and m.Maze.check_wall != "wall" or key == "w" and m.Maze.check_wall() != "wall":
+        if key == "Up" and m.Maze.check_wall() != "wall" or key == "w" and m.Maze.check_wall() != "wall":
             moves += 1
             self.object.move(0, -20)
-        if key == "Down" and m.Maze.check_wall != "wall" or key == "s" and m.Maze.check_wall() != "wall":
+        if key == "Down" and m.Maze.check_wall() != "wall" or key == "s" and m.Maze.check_wall() != "wall":
             moves += 1
             self.object.move(0, 20)
-        if key == "Left" and m.Maze.check_wall != "wall" or key == "a" and m.Maze.check_wall() != "wall":
+        if key == "Left" and m.Maze.check_wall() != "wall" or key == "a" and m.Maze.check_wall() != "wall":
             moves += 1
             self.object.move(-20, 0)
-        if key == "Right" and m.Maze.check_wall != "wall" or key == "d" and m.Maze.check_wall() != "wall":
+        if key == "Right" and m.Maze.check_wall() != "wall" or key == "d" and m.Maze.check_wall() != "wall":
             moves += 1
             self.object.move(20, 0)
+            
+    def get_location(self):
+        location = self.location
+        return location
+            
+w = g.GraphWin("Window", 1200, 1200)
+# bg must be a GIF
+background = g.Image((g.Point(600,600)), "corn_bg.gif")
+background.draw(w)
+
+
+
+p = Player(w, 300, 300)
+key = "z"
+while key != "q":
+    key = w.checkKey()
+    p.control(key)
+w.close()
