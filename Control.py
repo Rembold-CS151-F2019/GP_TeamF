2dc9f9907ed952b03d4ee88f0ef9ce6014d8912e
angriffith <54556780+angriffith@users.noreply.github.com>
2019-12-05 21:32:14 -0800

    Add files via upload

--- Control.py
+++ Control.py
@@ -11,18 +11,27 @@ import maze_class as m
 import Player_class as p
 
 class Level():
-    def __init__(self, level, window):
-        self.level = 1
-        self.w = window
+    def __init__(self, level_number):
+        self.levelcompleted = False
+        self.level_number = 1
+        maze = m.Maze()
+        w = g.GraphWin("Window", 1200, 1200)
+        # bg must be a GIF
+        background = g.Image((g.Point(600,600)), "corn_bg.gif")
+        background.draw(w)
+        scaling = 1200/m.Maze.get_name.dimensions
+
+        for row in range(m.Maze.get_name.dimensions):
+            for col in range(m.Maze.get_name.dimensions):
+                if m.Maze.check_wall(row, col) == "empty":
+                    path = g.Rectangle(g.Point(col * scaling, row * scaling), g.Point((col+1)*scaling, (row+1)*scaling))
+                    path.setFill("yellow")
+                    path.draw(self.w)
         
     def level_up(self):
-        self.level = 1
-        w = g.GraphWin('Maze', 800, 800)
-        w.setBackground('Green')
+        key = None
         
-        k=""
-        while k != "q":
-            clickpoint =w.checkMouse()
+        while key != "q" and self.levelcompleted == False:
             k=w.checkKey()
             
             player.control(k)
@@ -38,7 +47,7 @@ class Level():
         
         k=""
         while k != "q":
-            clickpoint =w.checkMouse()
+            
             k=w.checkKey()
             
             player.control(k)
@@ -47,21 +56,26 @@ class Level():
         
         if self.level > 5:
             return "You have escaped the final maze!"
-    def draw(self):
-        if self.level+1:
-            self.maze.draw(w)
-            w = g.GraphWin('Maze', 800,800)
-            w.setBackground('Green')
-            maze = m.Maze()
-            player = p.Player(w,0,0)
-            player.draw(w)
-            k=""
-            while k != "q":
-                clickpoint =w.checkMouse()
-                k=w.checkKey()
-                player.control(k)
     
-            w.close()     
+            
+
+w = g.GraphWin("Window", 1200, 1200)
+ # bg must be a GIF
+background = g.Image((g.Point(600,600)), "corn_bg.gif")
+background.draw(w)
+scaling = 1200/m.Maze.get_name.dimensions
+player = p.Player(w, 300, 300)
+key = "z"
+levelcount = 0
+while key != "q":
+    key = w.checkKey()
+    p.control(key)
+while levelcount < 6:
+    levelcount += 1
+    L = Level(levelcount)
+    if L.lost:
+        break
+w.close()
     
             
             
