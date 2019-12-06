e9753b3242e8073d073afd9b5df42f4724dd9ea1
angriffith <54556780+angriffith@users.noreply.github.com>
2019-12-05 23:17:55 -0800

    Add files via upload

--- Control.py
+++ Control.py
@@ -11,44 +11,46 @@ import maze_class as m
 import Player_class as p
 
 class Level():
-    def __init__(self, level_number):
+    def __init__(self, levelcount):
         self.levelcompleted = False
-        self.level_number = 1
-        maze = m.Maze()
+        self.levelcount = 1
+        
+        
         w = g.GraphWin("Window", 1200, 1200)
         # bg must be a GIF
         background = g.Image((g.Point(600,600)), "corn_bg.gif")
         background.draw(w)
         scaling = 1200/m.Maze().get_name.dimensions
+        player = p.Player(w, m.Maze.start[0], m.Maze.start[1])
 
-        for row in range(m.Maze.get_name.dimensions):
+        for row in range(m.Maze().get_name.dimensions):
             for col in range(m.Maze().get_name.dimensions):
-                if m.Maze.check_wall(row, col) == "empty":
+                if m.Maze().check_wall(row, col) == "empty":
                     path = g.Rectangle(g.Point(col * scaling, row * scaling), g.Point((col+1)*scaling, (row+1)*scaling))
                     path.setFill("yellow")
                     path.draw(self.w)
         
     def level_up(self):
         maze = m.Maze()
-        key = None
-        while key != "q" and self.levelcompleted ==False:
+        k = None
+        while k!= "q" and self.levelcompleted ==False:
             k=w.checkKey()
-            player.control(k)
-            if p.Player.location == maze.end:
+            p.control(k)
+            if p.Player.get_location() == maze.end:
                 self.levelcompleted == True
         
 
-w = g.GraphWin("Window", 1200, 1200)
+#w = g.GraphWin("Window", 1200, 1200)
  # bg must be a GIF
-background = g.Image((g.Point(600,600)), "corn_bg.gif")
-background.draw(w)
-scaling = 1200/m.Maze().get_name.dimensions
-player = p.Player(w, 300, 300)
-key = "z"
+#background = g.Image((g.Point(600,600)), "corn_bg.gif")
+#background.draw(w)
+#scaling = 1200/m.Maze().get_name.dimensions
+#player = p.Player(w, m.Maze.start[0], m.Maze.start[1])
+#key = "z"
 levelcount = 0
-while key != "q":
-    key = w.checkKey()
-    p.control(key)
+#while key != "q":
+    #key = w.checkKey()
+    #p.control(key)
 while levelcount < 6:
     levelcount += 1
     L = Level(levelcount)
